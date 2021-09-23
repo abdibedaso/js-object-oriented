c4ab9462d970ba3819050473e3e93d75343f1247
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-23 17:29:45 -0500

    Imp constructor creation

--- 3-Constructors/index.js
+++ 3-Constructors/index.js
@@ -2,20 +2,16 @@
 // An object is a collection of properties, and a property is an association between a name (or key) and a value. 
 // A property's value can be a function, in which case the property is known as a method. 
 
-// Factories/Constructor function
-function createCircle(radius) {
-
-    return {
-        radius,
-        draw: function() {
-            console.log('draw with radius:', radius);
-        }
+// Constructor function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw with radius:', this.radius);
     };
-
 }
 
-const circle1 = createCircle(1);
-circle1.draw();
-
-const circle2 = createCircle(2);
-circle2.draw();
+const circle = new Circle(1);
+circle.draw();
+// new - will create a new object
+// sets this to point to that object
+// return that object from that function
