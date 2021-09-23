54139e54d0cb04b5e091e14ce53126426da7186d
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-23 17:12:16 -0500

    create pull req - automerge workflows

---
+++ 3-Constructors/index.js
@@ -0,0 +1,21 @@
+// JavaScript is designed on a simple object-based paradigm. 
+// An object is a collection of properties, and a property is an association between a name (or key) and a value. 
+// A property's value can be a function, in which case the property is known as a method. 
+
+// Factories/Constructor function
+function createCircle(radius) {
+
+    return {
+        radius,
+        draw: function() {
+            console.log('draw with radius:', radius);
+        }
+    };
+
+}
+
+const circle1 = createCircle(1);
+circle1.draw();
+
+const circle2 = createCircle(2);
+circle2.draw();
